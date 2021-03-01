ea092d47e6204e08a4ba28ffcfd7be7e51959fcf
Md.Ghulam Azad Ansari <md.ghulamazadansari786@gmail.com>
2021-03-01 17:07:49 +0530

    debug true

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['e-commerce-6.herokuapp.com', '127.0.0.1']
 
